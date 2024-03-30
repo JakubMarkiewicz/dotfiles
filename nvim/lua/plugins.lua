c4c62febec3ecbfbc473baca4d37d252210e5d6d
JakubMarkiewicz <markiewicz.jakub2@gmail.com>
2024-03-30 13:47:57 +0100

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -35,6 +35,18 @@ lazy.setup({
 		end,
 	},
 
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		config = function()
+			require("CopilotChat").setup({})
+		end,
+	},
+
 	-- ollama
 	{ "David-Kunz/gen.nvim" },
 
